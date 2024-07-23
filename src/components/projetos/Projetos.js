a66846f88f71b96dc1e57c1f1fde0649726850fe
PedriSpengler <pedroj.oficial@gmail.com>
2024-07-23 16:58:26 -0400

    projects insert

--- src/components/projetos/Projetos.js
+++ src/components/projetos/Projetos.js
@@ -2,12 +2,26 @@ import "./Projetos.scss";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import {useEffect} from "react";
+import Publi1 from '../../assets/publi1.jpg';
+import Publi2 from '../../assets/publi2.jpg';
+import Publi3 from '../../assets/publi3.jpg';
 
 const Projetos = () => {
     return <section className="projetos section-padding">
         <div className="section-title projeto">
-            <h3>My Project</h3>
+            <h3>My Projects</h3>
             <span>We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers.</span>
+            <div className="info-projetos">
+                <div className="container">
+                    <img src={Publi1} alt="" />
+                </div>
+                <div className="container">
+                    <img src={Publi2} alt="" />
+                </div>
+                <div className="container">
+                    <img src={Publi3} alt="" />
+                </div>
+            </div>
         </div>
     </section>
 

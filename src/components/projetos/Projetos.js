cf10567252cc0e656c27018d6952372d2e381118
PedriSpengler <pedroj.oficial@gmail.com>
2024-07-24 10:29:00 -0400

    project finished

--- src/components/projetos/Projetos.js
+++ src/components/projetos/Projetos.js
@@ -10,6 +10,7 @@ import Seta from '../../assets/seta.png';
 const Projetos = () => {
     return (
         <section className="projetos section-padding">
+            <div className="lightshadow"></div>
             <div className="section-title projeto">
                 <h3>My Projects</h3>
                 <span>We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers.</span>

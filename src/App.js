9a8e10d19dc42eeeeaaca17d20e661e553da89a9
PedriSpengler <pedroj.oficial@gmail.com>
2024-10-16 21:36:27 -0400

    already2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
 import Resume from './components/resume/Resumo';
 import Servicos from './components/servicos/Servicos';
@@ -7,6 +6,7 @@ import Projetos from './components/projetos/Projetos';
 import Historia from './components/historia/Historia';
 import Work from './components/work/Work';
 import Rodape from './components/rodape/Rodape';
+import Navbar from './components/navbar/Navbar';
 
 
 

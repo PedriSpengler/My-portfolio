0f274f11d8d109ef4814a190f8d26f387c6af95e
PedriSpengler <pedroj.oficial@gmail.com>
2024-10-16 21:29:58 -0400

    finished

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -8,51 +8,51 @@ const Skills = () => {
             <h3>My Skills</h3>
             <div className="languages">
                 <div id="box">
-                    <img className="html" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" />
+                    <img className="html" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" alt=""/>
                     <span>HTML</span>
                 </div>
                 <div id="box">
-                    <img className="css" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" />    
+                    <img className="css" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" alt=""/>    
                     <span>CSS</span> 
                 </div>
                 <div id="box">
-                    <img className="js" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" />
+                    <img className="js" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" alt=""/>
                     <span>JS</span>
                 </div>
                 <div id="box">
-                    <img className="react" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" />
+                    <img className="react" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" alt=""/>
                     <span>REACT</span>
                 </div>
                 <div id="box">
-                    <img className="sass" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg" />
+                    <img className="sass" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg" alt=""/>
                     <span>SASS</span>
                 </div>
                 <div id="box">
-                    <img className="nodejs" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" />
+                    <img className="nodejs" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" alt="" />
                     <span>NODEJS</span>
                 </div>
                 <div id="box">
-                    <img className="csharp" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" />
+                    <img className="csharp" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" alt=""/>
                     <span>C#</span>
                 </div>
                 <div id="box">
-                    <img className="figma" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" />
+                    <img className="figma" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg" alt=""/>
                     <span>FIGMA</span>
                 </div>
                 <div id="box">
-                    <img className="git" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" />
+                    <img className="git" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" alt=""/>
                     <span>GIT</span>
                 </div>
                 <div id="box">
-                    <img className="python" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" />
+                    <img className="python" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" alt=""/>
                     <span>PYTHON</span>
                 </div>
                 <div id="box">
-                    <img className="sql" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" />
+                    <img className="sql" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" alt=""/>
                     <span>MYSQL</span>
                 </div>
                 <div id="box">
-                    <img className="uiux" src={ImgUIUX}/>
+                    <img className="uiux" src={ImgUIUX} alt=""/>
                     <span>UI/UX</span>
                 </div>
 

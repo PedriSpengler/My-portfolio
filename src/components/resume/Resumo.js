5b1fdeff0dc6491db45cf8763a5def60f00bf3e9
PedriSpengler <pedroj.oficial@gmail.com>
2024-10-23 20:12:53 -0400

    educ change

--- src/components/resume/Resumo.js
+++ src/components/resume/Resumo.js
@@ -40,25 +40,26 @@ const Resumo = () => {
       <h3 className="h3-edu">{t('resH3EDU')}</h3>
       <div className="info-education">
         <div className="edu-quadro quadro">
+          <h4>2024</h4>
+          <h5>PRO FIGMA - UI DESIGN</h5>
+          <h6 className="desc">Lucas Marte, Udemy</h6>
+        </div>
+        <div className="edu-quadro quadro">
+          <h4>2024</h4>
+          <h5>NLW EXPERT TILHA REACT NATIVE</h5>
+          <h6 class="desc">Rocketseat</h6>
+        </div>
+        <div className="edu-quadro quadro">
           <h4>2023</h4>
           <h5>JAVA COM SPRING BOOT</h5>
           <h6 className="desc">Rocketseat</h6>
         </div>
-          <div className="edu-quadro quadro">
-            <h4>2024</h4>
-            <h5>NLW EXPERT TILHA REACT NATIVE</h5>
-            <h6 class="desc">Rocketseat</h6>
-          </div>
-          <div className="edu-quadro quadro">
-            <h4>2023</h4>
-            <h5>GO-LANG INTRODUÇÃO E USO</h5>
-            <h6 className="desc">SIC</h6>
-          </div>
-          <div className="edu-quadro quadro">
-            <h4>2022-2023</h4>
-            <h5>PRO FIGMA - UI DESIGN</h5>
-            <h6 className="desc">Lucas Marte, Udemy</h6>
-          </div>
+        <div className="edu-quadro quadro">
+          <h4>2023</h4>
+          <h5>GO-LANG INTRODUÇÃO E USO</h5>
+          <h6 className="desc">SIC</h6>
+        </div>
+          
         </div>
     </div>
   </section>
